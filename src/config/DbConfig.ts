3944dd8302aa2ad489f5743a18d56503fa775e59
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-02-16 20:48:36 +0300

    add

--- src/config/DbConfig.ts
+++ src/config/DbConfig.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
     try {
-        const mongoUrl = process.env.MONGOURL || "";
+        const mongoUrl = process.env.mongourl || "";
         await mongoose.connect(mongoUrl);
         console.log("MongoDB Connection Successfull");
     } catch (error) {
